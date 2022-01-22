dd0b90fbc04b95351fbc307abf84bd12743ba129
lausamtuong <lausamtuong@gmail.com>
2022-01-22 12:55:35 +0700

    sdasd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 dotenv.config()
-const PORT = 30000
+const PORT = process.env.PORT||30000
 const server = app.listen(PORT,() => {
     console.log(`Listening on PORT: ${PORT}`)
 })
